209cfe2077d469c4818f234b0806d1f0522cb5c9
EdwinSch <edwinschutjes@gmail.com>
2023-08-11 16:21:41 +0200

    replace local data for fetched data

--- src/context.jsx
+++ src/context.jsx
@@ -14,9 +14,11 @@ import {
 
 const AppContext = createContext();
 
+const url = "https://www.course-api.com/react-useReducer-cart-project";
+
 const initialState = {
   loading: false,
-  cart: new Map(cartItems.map((item) => [item.id, item])),
+  cart: new Map(),
 };
 
 // console.log(initialState.cart);
@@ -40,6 +42,16 @@ export const AppProvider = ({ children }) => {
   const decrease = (id) => {
     dispatch({ type: DECREASE, payload: { id } });
   };
+
+  const fetchData = async () => {
+    dispatch({ type: LOADING });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: DISPLAY_ITEMS, payload: { cart } });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <AppContext.Provider
